ae87dd761faf8b5c3701b545b94b87cf5e56848d
Dan Aharon <dan@danielaharon.com>
2020-04-19 11:48:15 +0300

    Trivial syntax highlighting changes.

--- src/main/kotlin/org/ocaml/ide/colorscheme/OcamlColorSettingsPage.kt
+++ src/main/kotlin/org/ocaml/ide/colorscheme/OcamlColorSettingsPage.kt
@@ -5,63 +5,71 @@ import com.intellij.openapi.fileTypes.SyntaxHighlighter
 import com.intellij.openapi.options.colors.AttributesDescriptor
 import com.intellij.openapi.options.colors.ColorDescriptor
 import com.intellij.openapi.options.colors.ColorSettingsPage
+
 import org.ocaml.ide.highlighter.OcamlSyntaxHighlighter
 import org.ocaml.lang.OcamlIcons
+
 import javax.swing.Icon
 
 /**
  * Created by sidharthkuruvila on 04/05/16.
  */
 class OcamlColorSettingsPage : ColorSettingsPage {
-    override fun getIcon(): Icon? {
-        return OcamlIcons.MODULE_ICON_FILE
-    }
-
-    override fun getAdditionalHighlightingTagToDescriptorMap(): MutableMap<String, TextAttributesKey>? {
-        return null
-    }
 
     companion object {
         val DESCRIPTORS = arrayOf(
-                AttributesDescriptor("Brace", OcamlSyntaxHighlighter.BRACE),
-                AttributesDescriptor("Bracket", OcamlSyntaxHighlighter.BRACKET),
-                AttributesDescriptor("Paren", OcamlSyntaxHighlighter.PAREN),
-                AttributesDescriptor("Number", OcamlSyntaxHighlighter.NUMBER),
-                AttributesDescriptor("Uppercase Identifier", OcamlSyntaxHighlighter.UIDENTIFIER),
-                AttributesDescriptor("Lowercase Identifier", OcamlSyntaxHighlighter.IDENTIFIER),
-                AttributesDescriptor("Keyword", OcamlSyntaxHighlighter.KEYWORD),
-                AttributesDescriptor("String", OcamlSyntaxHighlighter.STRING),
-                AttributesDescriptor("Comment", OcamlSyntaxHighlighter.COMMENT),
-                AttributesDescriptor("Operator", OcamlSyntaxHighlighter.OPERATOR)
+            AttributesDescriptor("Brace", OcamlSyntaxHighlighter.BRACE),
+            AttributesDescriptor("Bracket", OcamlSyntaxHighlighter.BRACKET),
+            AttributesDescriptor("Paren", OcamlSyntaxHighlighter.PAREN),
+            AttributesDescriptor("Number", OcamlSyntaxHighlighter.NUMBER),
+            AttributesDescriptor("Uppercase Identifier", OcamlSyntaxHighlighter.UIDENTIFIER),
+            AttributesDescriptor("Lowercase Identifier", OcamlSyntaxHighlighter.IDENTIFIER),
+            AttributesDescriptor("Keyword", OcamlSyntaxHighlighter.KEYWORD),
+            AttributesDescriptor("String", OcamlSyntaxHighlighter.STRING),
+            AttributesDescriptor("Comment", OcamlSyntaxHighlighter.COMMENT),
+            AttributesDescriptor("Documentation Comment", OcamlSyntaxHighlighter.DOC_COMMENT),
+            AttributesDescriptor("Operator", OcamlSyntaxHighlighter.OPERATOR)
         )
     }
 
-    override fun getHighlighter(): SyntaxHighlighter {
-        return OcamlSyntaxHighlighter();
-    }
+    override fun getIcon(): Icon? = OcamlIcons.MODULE_ICON_FILE
 
-    override fun getDemoText(): String {
-        return """
-        (* Ocaml works *)
+    override fun getAdditionalHighlightingTagToDescriptorMap(): MutableMap<String, TextAttributesKey>? = null
+
+    override fun getHighlighter(): SyntaxHighlighter = OcamlSyntaxHighlighter()
+
+    override fun getDemoText(): String = """
         open Str
+        
+        (* Example comment *)
         let x = 4 + 3
-        let y = 13
-        let a = [| 1; 3;|]
-        let b = (4, 5)
-        """;
-    }
+        let y = [ "one"; "two" ]
+        let z = [| 1; 3;|]
+        let a = (4, 5)
+        Printf.printf "Number:  %d\n" 10;
+        print_endline "String:  %s\n" "example";
+        
+        (** Example documentation comment *)
+        module type Module_type = sig
+            type t
+            val make : 'a -> 'a list -> t
+        end
+        
+        module Module_impl = struct
+            type 'a t = {
+                x : 'a;
+                y : 'a list;
+            }
+            let make x y = { ~x; ~y }
+        end
+        
+        module Make (M : Module_type) : Module_impl with type t = M.t = struct
+            include M
+        end""".trimIndent()
 
+    override fun getAttributeDescriptors(): Array<AttributesDescriptor> = DESCRIPTORS
 
-    override fun getAttributeDescriptors(): Array<AttributesDescriptor> {
-        return DESCRIPTORS;
-    }
+    override fun getColorDescriptors(): Array<ColorDescriptor> = ColorDescriptor.EMPTY_ARRAY
 
-    override fun getColorDescriptors(): Array<ColorDescriptor> {
-        return ColorDescriptor.EMPTY_ARRAY;
-    }
-
-    override fun getDisplayName(): String {
-        return "Ocaml";
-    }
-
+    override fun getDisplayName(): String = "Ocaml"
 }
